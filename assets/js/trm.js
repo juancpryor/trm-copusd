1919a214a5378fc3365c224b51752223e6a7f146
Juan C. Pryor <juancpryor@yahoo.com>
2020-05-28 23:29:13 -0500

    Updated - Lookup previous value when 0
    Weekend and non-banking days provide the same value as the previous day. Tried a lot to do this with do....while but to no avail. 
    
    Nested ifs may not be that elegant but the program is working.

--- assets/js/trm.js
+++ assets/js/trm.js
@@ -1,39 +1,130 @@
 // Add app key provided by datos.gov portal
 
-const datosKey = '8QDVstFtyXNyOSESJqn2ucpVN';
+const datosKey = 'xxxxxxxxxxxxxxxxxxx';
 
 // API provided by datos.gov portal for TRM
 
 const datosTRMURI = 'https://www.datos.gov.co/resource/mcec-87by.json';
 
-// Request DOM for date and UI
-const placeTRM = document.querySelector('#trm');
-const fechaForm = document.querySelector('form');
+// Get information from datos.gov.co portal for the submitted date
 
-fechaForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const fecha = fechaForm.fecha.value.trim();
-    fechaForm.reset();
-    const query = datosTRMURI + `?vigenciadesde=` + fecha;
-    
+$('form').on('submit', function(event) {
+    event.preventDefault();
+    let fechaSolicitada = $('form').find('input[name="fecha"]').val().trim();
+    let fecha = fechaSolicitada;
+    let query = `https://www.datos.gov.co/resource/mcec-87by.json?vigenciadesde=` + fecha;
+    document.querySelector('form').reset();    
+    // Update UI and show contrainer with data    
+    $("#container-trm").fadeIn(2000).removeClass("d-none");
+
     $.ajax({
         url: query,
         type: "GET",
         data: {
-          "$limit" : 100,
-          "$$app_token" : datosKey
+            "$$app_token" : datosKey
         }
     }).done(function(data) {
-        // return data[0];
-        // alert("Retrieved " + data.length + " records from the dataset!");
-        const result = data.map(a => a.valor.toString());
-        console.log(data);
-        console.log(result);
-        placeTRM.innerHTML = `<div class="my-3" id="fechaTRM">La TRM para ${fecha} es de</div>
-                <div class="display-4 my-4">
-                    <span>COP</span>
-                    <span>${result}</span>
-                </div>`;
+        
+        // 4 nested ifs for weekend and non-banking holidays
+                
+        if(data.map(a => a.valor.toString()).length === 1){
+            const resultTRM = data.map(a => a.valor.toString());
+            $("#container-trm").fadeIn(2000).removeClass("d-none");
+            $("#trm").html(`<div class="my-3" id="fechaTRM">La TRM para ${fechaSolicitada} es de</div>
+                    <div class="display-4 my-4">
+                        <span>COP</span>
+                        <span>${resultTRM}</span>
+                    </div>`);
+        }   else{    
+            fecha = dateFns.addDays(dateFns.parse(fecha), -1);
+            fecha = dateFns.format(fecha, 'YYYY-MM-DD');
+            query = `https://www.datos.gov.co/resource/mcec-87by.json?vigenciadesde=` + fecha;
+            $.ajax({
+                url: query,
+                type: "GET",
+                data: {
+                    "$$app_token" : datosKey
+                }
+            }).done(function(data) {
+                if(data.map(a => a.valor.toString()).length === 1){
+                    const resultTRM = data.map(a => a.valor.toString());
+                    $("#container-trm").fadeIn(2000).removeClass("d-none");
+                    $("#trm").html(`<div class="my-3" id="fechaTRM">La TRM para ${fechaSolicitada} es de</div>
+                            <div class="display-4 my-4">
+                                <span>COP</span>
+                                <span>${resultTRM}</span>
+                            </div>`);
+                } else {
+                    fecha = dateFns.addDays(dateFns.parse(fecha), -1);
+                    fecha = dateFns.format(fecha, 'YYYY-MM-DD');
+                    query = `https://www.datos.gov.co/resource/mcec-87by.json?vigenciadesde=` + fecha;
+                        $.ajax({
+                        url: query,
+                        type: "GET",
+                        data: {
+                            "$$app_token" : datosKey
+                        }
+                    }).done(function(data) {
+                        if(data.map(a => a.valor.toString()).length === 1){
+                            const resultTRM = data.map(a => a.valor.toString());
+                            $("#container-trm").fadeIn(2000).removeClass("d-none");
+                            $("#trm").html(`<div class="my-3" id="fechaTRM">La TRM para ${fechaSolicitada} es de</div>
+                                    <div class="display-4 my-4">
+                                        <span>COP</span>
+                                        <span>${resultTRM}</span>
+                                    </div>`);
+                        } else {
+                            fecha = dateFns.addDays(dateFns.parse(fecha), -1);
+                            fecha = dateFns.format(fecha, 'YYYY-MM-DD');
+                            query = `https://www.datos.gov.co/resource/mcec-87by.json?vigenciadesde=` + fecha;
+                            $.ajax({
+                                url: query,
+                                type: "GET",
+                                data: {
+                                    "$$app_token" : datosKey
+                                }
+                            }).done(function(data) {
+                                if(data.map(a => a.valor.toString()).length === 1){
+                                    const resultTRM = data.map(a => a.valor.toString());
+                                    $("#container-trm").fadeIn(2000).removeClass("d-none");
+                                    $("#trm").html(`<div class="my-3" id="fechaTRM">La TRM para ${fechaSolicitada} es de</div>
+                                            <div class="display-4 my-4">
+                                                <span>COP</span>
+                                                <span>${resultTRM}</span>
+                                            </div>`);
+                                } else {
+                                        fecha = dateFns.addDays(dateFns.parse(fecha), -1);
+                                        fecha = dateFns.format(fecha, 'YYYY-MM-DD');
+                                        query = `https://www.datos.gov.co/resource/mcec-87by.json?vigenciadesde=` + fecha;
+                                        $.ajax({
+                                            url: query,
+                                            type: "GET",
+                                            data: {
+                                                "$$app_token" : datosKey
+                                            }
+                                        }).done(function(data) {
+                                            if(data.map(a => a.valor.toString()).length === 1){
+                                                const resultTRM = data.map(a => a.valor.toString());
+                                                $("#container-trm").fadeIn(2000).removeClass("d-none");
+                                                $("#trm").html(`<div class="my-3" id="fechaTRM">La TRM para ${fechaSolicitada} es de</div>
+                                                        <div class="display-4 my-4">
+                                                            <span>COP</span>
+                                                            <span>${resultTRM}</span>
+                                                        </div>`);
+                                            } else {
+                                                console.log('There seems to be a mistake')
+                                            };
+                                        });    
+                                    //
+                                };
+                            });            
+
+                            //
+                        };
+                    
+                        });
+                };
+            });
+        };
     });
-    console.log(fecha);
 });
